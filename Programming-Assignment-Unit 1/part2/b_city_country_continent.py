397f2425155641727c4575dcd91012af6d8b5846
Sara <sarahsulokoka@gmail.com>
2025-09-06 22:19:38 +0300

    city-country-continet added

---
+++ Programming-Assignment-Unit 1/part2/b_city_country_continent.py
@@ -0,0 +1,13 @@
+# Part 2(b): Display the name of the city, country, and continent
+print("Athens, Greece, Europe")
+
+print('Athens', 'Greece' , 'Europe')
+
+print('city: Athens')
+print('Country: Greece')
+print('continet: Europe')
+
+print(' City: Athens \n Country: Greece \n Continet: Europe')
+
+# What I learned:
+# I learned that the print() function is versatile. I can pass it a single, formatted string to display information exactly as I want it to appear. This is an efficient way to output static text for the user.Sorry for the too many option, i was playing to check the format.
