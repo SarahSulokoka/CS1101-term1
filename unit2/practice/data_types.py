0a02f46ea5e1f2f53e6d44fee53a20694db7be88
Sara <sarahsulokoka@gmail.com>
2025-09-13 20:54:29 +0300

    string added to data_types file

--- unit2/practice/data_types.py
+++ unit2/practice/data_types.py
@@ -1,12 +1,24 @@
-a = -6
+
+#INTEGERS
+
+a = 9
 b = a * a - a -1
 c = a * b
 print(c)
 
 if (a<0):
+    print ("a<0")
     print(c)
 else:
     print("a is not less than 0")
     print(c-a)
 
 print("OK")
+
+
+# //STRING
+
+
+a = "My first test string"
+b = 'Another test string that I have defined'
+c = " This is Sal's string"
