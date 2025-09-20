5872100eb9da933a0e72643e1cdd51d0c51188a3
Sara <sarahsulokoka@gmail.com>
2025-09-20 15:42:08 +0300

    fix: saratur updated

--- unit3/saratur.py
+++ unit3/saratur.py
@@ -1,11 +1,21 @@
 import turtle
 
-def square(t):
+# def square(t):
+#     for i in range(4):
+#         t.forward(100)   # move forward 100 units
+#         t.left(90)       # turn left 90 degrees
+
+# sara = turtle.Turtle()
+# square(sara)
+
+# turtle.done()
+
+
+
+def square(t, length):
     for i in range(4):
-        t.forward(100)   # move forward 100 units
-        t.left(90)       # turn left 90 degrees
+        t.forward(length)
+        t.left(90)
 
-sara = turtle.Turtle()
-square(sara)
-
-turtle.done()
+bob = turtle.Turtle()
+square(bob, 150)   # try different lengths
